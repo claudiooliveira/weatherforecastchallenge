a1464de0599d218abbb75f1acfb6d87ce12f3b5b
Claudio Oliveira <claudio.oliveira@outlook.com.br>
2020-01-11 03:28:08 -0300

    fixed darksky prefs key

--- app/src/main/java/com/example/weatherforecastchallenge/Consts.kt
+++ app/src/main/java/com/example/weatherforecastchallenge/Consts.kt
@@ -4,7 +4,7 @@ class Consts {
     companion object {
 
         val PREFS_FILENAME = "weatherforecastchallenge.prefs"
-        val DARK_SKY_KEY = "fefe7e6b0fc373c40fadab9e2a36c87b"
+        val DARK_SKY_KEY = "dark_sky_key"
 
     }
 }
