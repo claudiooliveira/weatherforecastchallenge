a852fd2fed57b0a0ab5ea1c8f4b3a81216aa23ca
Claudio Oliveira <claudio.oliveira@outlook.com.br>
2020-01-13 21:57:25 -0300

    chore(forecast): We now request the user's location to display the weather forecast for their current location.

--- app/src/main/java/com/example/weatherforecastchallenge/Consts.kt
+++ app/src/main/java/com/example/weatherforecastchallenge/Consts.kt
@@ -5,6 +5,7 @@ class Consts {
 
         val PREFS_FILENAME = "weatherforecastchallenge.prefs"
         val DARK_SKY_KEY = "dark_sky_key"
+        val GEOLOCATION_PERMISSIONS_ID = 45501
 
     }
 }
--- app/src/main/java/com/example/weatherforecastchallenge/home/HomeFragment.kt
+++ app/src/main/java/com/example/weatherforecastchallenge/home/HomeFragment.kt
@@ -1,8 +1,13 @@
 package com.example.weatherforecastchallenge.home
 
 
+import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
@@ -12,12 +17,16 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
 import android.widget.TextView
+import com.example.weatherforecastchallenge.Consts
 
 import com.example.weatherforecastchallenge.R
 import com.example.weatherforecastchallenge.utils.DateUtils
 import com.example.weatherforecastchallenge.weather.WeatherDataModel
 import com.example.weatherforecastchallenge.weather.WeatherModel
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import kotlinx.android.synthetic.main.forecast_day_item.view.*
+import java.lang.Exception
 import java.util.*
 
 /**
@@ -36,6 +45,10 @@ class HomeFragment : Fragment(), HomeFragmentInput, HomeView {
     lateinit var txtDegrees : TextView
     lateinit var txtTitle : TextView
     lateinit var rvNextDays : RecyclerView
+    var latitude : Double = 0.0
+    var longitude : Double = 0.0
+
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -49,10 +62,42 @@ class HomeFragment : Fragment(), HomeFragmentInput, HomeView {
         //Bind views...
         bindViews(view)
 
+        //Request user location
+        requestUserLocation()
+
         //Fetch weather data
         fetchData()
 
         return view
+    }
+
+    @SuppressLint("MissingPermission")
+    fun requestUserLocation() {
+        if (checkPermissions()) {
+            fusedLocationClient = LocationServices.getFusedLocationProviderClient(context!!)
+            fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
+                latitude = location!!.latitude
+                longitude = location!!.longitude
+                fetchData()
+            }
+        }else{
+            requestPermissions()
+        }
+    }
+
+    private fun checkPermissions(): Boolean {
+        if (ActivityCompat.checkSelfPermission(context!!, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(context!!, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            return true
+        }
+        return false
+    }
+
+    private fun requestPermissions() {
+        requestPermissions(
+            arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION),
+            Consts.GEOLOCATION_PERMISSIONS_ID
+        )
     }
 
     fun bindViews(view : View) {
@@ -62,11 +107,12 @@ class HomeFragment : Fragment(), HomeFragmentInput, HomeView {
     }
 
     fun fetchData() {
-        val request = WeatherForecastRequest(-24.0368638, -46.5370516)
+        val request = WeatherForecastRequest(latitude, longitude)
         output.fetchWeatherData(context!!, request)
     }
 
     override fun displayWeatherData(weatherData : WeatherModel) {
+
         this.weatherData = weatherData
 
         //Display current date
@@ -95,6 +141,14 @@ class HomeFragment : Fragment(), HomeFragmentInput, HomeView {
         var forecastNextDays = weatherData.daily!!.data
         rvNextDays.adapter = ForecastNextDaysAdapter(forecastNextDays.drop(1), context!!) //Since the challenge requires displaying the forecast next 7 days, I used list.drop (1) to remove the first item, because the API returns the current day in this array as well.
         rvNextDays.layoutManager = LinearLayoutManager(context!!, LinearLayout.VERTICAL, false)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        if (requestCode == Consts.GEOLOCATION_PERMISSIONS_ID) {
+            if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                requestUserLocation()
+            }
+        }
     }
 
     /*
